899bc32041ade4632096df8af411f8a46b85a7b0
mrpiyalst <mrpiyalst@gmail.com>
2026-05-14 22:57:34 +0600

    Create Agency Project

---
+++ Agency/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <>
+        <div id='navbar'>
+            <div id='wrapper'>
+                <div>
+                    <div className='bg-red-400'>
+                        <img src={logo} alt="logo_error" />
+                    </div>
+                    <div>
+                        <li>
+                            
+                        </li>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </>
+  )
+}
+
+export default Navbar
